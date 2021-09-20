62938e7ab572b92996e2427188178b840342aad0
Pompe <Pompedev@gmail.com>
2021-09-20 22:03:18 +0200

    Add Binding to TextBlock Add Command to Button Add show digit from button in Textblock

--- AvaloniaCalculator/ViewModels/MainWindowViewModel.cs
+++ AvaloniaCalculator/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AvaloniaCalculator.ViewModels {
-    public class MainWindowViewModel : ViewModelBase {
+    public class MainWindowViewModel : INotifyPropertyChanged{
+        private string _textFieldText = "";
         public string Greeting => "Welcome to Avalonia!";
+        public string FieldText {
+            get => _textFieldText;
+            set {
+                _textFieldText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FieldText)));
+            }
+        }
+
+        public void ButtonClicked(string input) => FieldText = input;
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
