b057796f7fca7012e24bc1d8595fb5bc6f1531d2
Pompe <Pompedev@gmail.com>
2021-09-23 19:25:01 +0200

    Add basic shunting yard Add commands to each button add commandParameter to each button

--- AvaloniaCalculator/ViewModels/MainWindowViewModel.cs
+++ AvaloniaCalculator/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 
 namespace AvaloniaCalculator.ViewModels {
-    public class MainWindowViewModel : INotifyPropertyChanged{
+    public class MainWindowViewModel : INotifyPropertyChanged {
         private string _textFieldText = "";
         public string Greeting => "Welcome to Avalonia!";
         public string FieldText {
@@ -15,7 +16,61 @@ namespace AvaloniaCalculator.ViewModels {
             }
         }
 
-        public void ButtonClicked(string input) => FieldText = input;
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void ButtonClicked(string input) => FieldText = EvaluatedExpressionString(input);
+
+        private string EvaluatedExpressionString(string input) {
+            var outputQueue = new Queue<string>();
+            var operatorStack = new Stack<string>();
+
+            for (var i = 0; i < FieldText.Length; i++) {
+                var c = FieldText[i];
+
+                if (char.IsDigit(c)) {
+                    outputQueue.Enqueue(c.ToString());
+                }
+
+                if (c is '+' or '-' or '*' or '/') {
+                    while (operatorStack.Count > 0
+                           && operatorStack.Peek() != "("
+                           && Precedence(operatorStack.Peek()) > Precedence(c.ToString())) {
+                        outputQueue.Enqueue(operatorStack.Pop());
+                    }
+
+                    operatorStack.Push(c.ToString());
+                }
+
+                if (c == '(') {
+                    operatorStack.Push(c.ToString());
+                }
+                else if (c == ')') {
+                    while (operatorStack.Count != 0 && operatorStack.Peek() != "(") {
+                        outputQueue.Enqueue(operatorStack.Pop());
+                    }
+
+                    if (operatorStack.Count != 0 && operatorStack.Peek() == ")") {
+                        operatorStack.Pop();
+                    }
+                }
+            }
+
+            while (operatorStack.Count != 0) {
+               outputQueue.Enqueue(operatorStack.Pop()); 
+            }
+
+            var test = "";
+            foreach (var t in outputQueue) {
+                test += t;
+            }
+            
+            Debug.WriteLine(test);
+
+            return FieldText + input;
+        }
+
+        private static int Precedence(string c) {
+            return c is "-"or "+" ? 1 : 2;
+        }
     }
 }
